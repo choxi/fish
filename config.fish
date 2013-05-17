9c3128b5a5a46edca82ef1fbc4b403ff32c9a717
Roshan Choxi <roshan.choxi@gmail.com>
2013-05-17 16:43:49 -0700

    first commit

---
+++ config.fish
@@ -0,0 +1,20 @@
+set PATH /usr/local/bin $PATH
+
+set PATH $PATH /usr/local/mysql/bin
+set DEPLOYER=roshan
+
+set PATH $PATH $HOME/.rvm/bin # Add RVM to PATH for scripting
+set PATH /Applications/Postgres.app/Contents/MacOS/bin $PATH # Add Postgres.app to PATH
+
+alias gs  "git status"
+alias gca "git commit -a"
+alias gc  "git commit"
+alias gco "git checkout"
+
+function c
+  cd ~/Code/$argv
+end
+
+function h
+  cd ~/$argv
+end
