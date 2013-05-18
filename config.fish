c2b95aab3811080260e1e71426c74fc78d107011
Roshan Choxi <roshan.choxi@gmail.com>
2013-05-17 17:22:46 -0700

    tab command

--- config.fish
+++ config.fish
@@ -4,6 +4,8 @@ set PATH $PATH /usr/local/mysql/bin
 set DEPLOYER=roshan
 
 set PATH $PATH $HOME/.rvm/bin # Add RVM to PATH for scripting
+rvm > /dev/null # load RVM environment
+
 set PATH /Applications/Postgres.app/Contents/MacOS/bin $PATH # Add Postgres.app to PATH
 
 alias gs  "git status"
@@ -17,4 +19,11 @@ end
 
 function h
   cd ~/$argv
+end
+
+function tab
+  osascript -e "tell application \"Terminal\"" \
+      -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+      -e "do script \"cd $PWD \" in front window" \
+      -e "end tell"
 end
