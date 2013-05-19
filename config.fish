215074f8ac5843e9caa62ddfd356a8478e4f2c7c
Roshan Choxi <roshan.choxi@gmail.com>
2013-05-19 16:33:31 -0700

    add bin to PATH

--- config.fish
+++ config.fish
@@ -7,6 +7,7 @@ set PATH $PATH $HOME/.rvm/bin # Add RVM to PATH for scripting
 rvm > /dev/null # load RVM environment
 
 set PATH /Applications/Postgres.app/Contents/MacOS/bin $PATH # Add Postgres.app to PATH
+set PATH bin $PATH
 
 alias gs  "git status"
 alias gca "git commit -a"
