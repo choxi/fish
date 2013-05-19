e69c98a6a97e97e1e40e61c635b675378968679a
Roshan Choxi <roshan.choxi@gmail.com>
2013-05-19 16:34:04 -0700

    add git status to prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,17 @@
+# Fish git prompt
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+
 function fish_prompt
+  set last_status $status
+
   set_color $fish_color_cwd
-  echo -n (basename $PWD)
+  printf '%s' (basename (prompt_pwd))
   set_color normal
-  echo -n ' ) '
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
 end
