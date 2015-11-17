8c82384007c51b172e2c708573384fe1e0d1a03a
Roshan Choxi <roshan.choxi@gmail.com>
2015-11-17 15:54:06 -0800

    code shortcut

--- config.fish
+++ config.fish
@@ -1,21 +1,25 @@
+set PATH /usr/bin $PATH
 set PATH /usr/local/bin $PATH
-
 set PATH $PATH /usr/local/mysql/bin
+set PATH $PATH /usr/local/share/npm/bin/
+set NODE_PATH $NODE_PATH /Users/roshan/Code/bloc/javascript-source-exercises/node_modules
+
 set DEPLOYER=roshan
 
-set PATH $PATH $HOME/.rvm/bin # Add RVM to PATH for scripting
-rvm > /dev/null # load RVM environment
-
-set PATH /Applications/Postgres.app/Contents/MacOS/bin $PATH # Add Postgres.app to PATH
+set PATH /Applications/Postgres93.app/Contents/MacOS/bin $PATH # Add Postgres.app to PATH
 set PATH bin $PATH
+
+rvm > /dev/null # load RVM environment
 
 alias gs  "git status"
 alias gca "git commit -a"
 alias gc  "git commit"
 alias gco "git checkout"
+alias sync "bundle; and rake db:migrate; and rake db:test:prepare"
+alias deploy "git push; and git push heroku master"
 
 function c
-  cd ~/Code/$argv
+  cd ~/roshan/Code/$argv
 end
 
 function h
